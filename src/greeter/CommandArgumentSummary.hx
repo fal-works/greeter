d0950c6909f127924752488f45d1bae978fee837
FAL <falworks.contact@gmail.com>
2020-06-11 15:51:52 +0900

    add methods in CommandArgumentSummary

--- src/greeter/CommandArgumentSummary.hx
+++ src/greeter/CommandArgumentSummary.hx
@@ -65,18 +65,19 @@ class CommandArgumentSummary {
 	public final optionValuesMap: CommandOptionValuesMap;
 
 	/**
-		Returns the first command value.
-		Throws error if zero or multiple command values were provided to the command.
+		Gets the value assuming that just one value has been passed to the command.
+		@return The first command value.
+		Throws error if zero or multiple command values have been passed.
 	**/
-	public extern inline function getSingleCommandValue(): String {
+	public extern inline function oneCommandValue(): String {
 		final values = this.commandValues;
 		switch values.length {
 			case 0:
-				throw 'Found no command value.';
+				throw 'Passed no command value.';
 			case 1:
 				return values[0];
 			default:
-				throw 'Too many command values.';
+				throw 'Passed too many command values.';
 		}
 	}
 
@@ -112,6 +113,7 @@ class CommandArgumentSummary {
 
 /**
 	Mapping from each option to a list of values provided for the option.
+	Provides methods for getting values with cardinality checks.
 **/
 @:forward
 abstract CommandOptionValuesMap(
@@ -121,13 +123,16 @@ abstract CommandOptionValuesMap(
 		this = new Map();
 
 	/**
-		Returns the first value for `option`.
-		Throws error if `option` is not found, or if zero or multiple values were provided for `option`.
+		Gets the value for `option` assuming that only one value is provided.
+		- Use `exists()` for just checking if `option` has been passed regardless of its values.
+		- Use `zeroOrOne()` if the value is optional for `option`.
+		@return The first value for `option`, or `Maybe.none()` if `option` itself is not found.
+		Throws error if zero or multiple values have been provided for `option`.
 	**/
-	public extern inline function getSingle(option: CommandOption): String {
+	public function one(option: CommandOption): Maybe<String> {
 		final values = this.get(option);
 		if (values == null)
-			throw 'Option not found: ${option.toString()}';
+			return Maybe.none();
 
 		switch values.length {
 			case 0:
@@ -140,9 +145,69 @@ abstract CommandOptionValuesMap(
 	}
 
 	/**
-		@return Values provided for `option` in `Maybe` representation.
-		`Maybe.none()` if `option` is not found.
+		Gets the value for `option` assuming that only zero or one value is provided.
+		@return The first value for `option`, or `Zero` if no value, or `Maybe.none()` if `option` is not found.
+		Throws error if multiple values have been provided for `option`.
 	**/
-	public extern inline function tryGet(option: CommandOption): Maybe<Array<String>>
+	public function zeroOrOne(option: CommandOption): Maybe<ZeroOrOne> {
+		final values = this.get(option);
+		if (values == null)
+			return Maybe.none();
+
+		switch values.length {
+			case 0:
+				return Maybe.from(ZeroOrOne.Zero);
+			case 1:
+				return Maybe.from(ZeroOrOne.One(values[0]));
+			default:
+				throw 'Too many values provided for option: ${option.toString()}';
+		}
+	}
+
+	/**
+		Gets the values for `option` assuming that at least one value is provided.
+		@return Values provided for `option`, or `Maybe.none()` if `option` itself is not found.
+	**/
+	public function oneOrMore(option: CommandOption): Maybe<Array<String>> {
+		final values = this.get(option);
+		if (values != null && values.length == 0)
+			throw 'No value provided for option: ${option.toString()}';
+		return Maybe.from(values).or([]);
+	}
+
+	/**
+		Gets the values for `option`.
+		@return Values provided for `option`, or `Maybe.none()` if `option` itself is not found.
+	**/
+	public function zeroOrMore(option: CommandOption): Maybe<Array<String>>
 		return Maybe.from(this.get(option));
+}
+
+private enum ZeroOrOne {
+	/**
+		Represents that no value has been provided.
+	**/
+	Zero;
+
+	/**
+		Represents that just one value has been provided.
+	**/
+	One(value: String);
+}
+
+private enum ZeroOrMore {
+	/**
+		Represents that no value has been provided.
+	**/
+	Zero;
+
+	/**
+		Represents that just one value has been provided.
+	**/
+	One(value: String);
+
+	/**
+		Represents that more than one values have been provided.
+	**/
+	More(values: Array<String>);
 }
