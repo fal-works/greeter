1e8bda12c1b902ab96a3e4a93207bdee15b4b907
FAL <falworks.contact@gmail.com>
2020-06-11 20:29:46 +0900

    add CommandLineInterfaceType.getCli()

--- src/greeter/CommandLineInterfaceType.hx
+++ src/greeter/CommandLineInterfaceType.hx
@@ -4,6 +4,16 @@ package greeter;
 	CLI type that can be used for `switch` expressions.
 **/
 enum abstract CommandLineInterfaceType(Int) {
+	/**
+		@return The CLI instance which `this` corresponds to.
+	**/
+	public inline function getCli(): CommandLineInterface {
+		return switch (cast this: CommandLineInterfaceType) {
+			case Unix: CommandLineInterfaceSet.unix;
+			case Dos: CommandLineInterfaceSet.dos;
+		}
+	};
+
 	final Unix;
 	final Dos;
 }
